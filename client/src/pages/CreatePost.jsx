b50cd667e29d185deae06b2e725aaf6d7a55903e
Pratik-Nawale <pratiknawale03@gmail.com>
2023-03-04 18:34:14 +0530

    initial commit, create home page

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../Components";
+
+const CreatePost = () => {
+  return <div>CreatePost</div>;
+};
+
+export default CreatePost;
